031a7932f2b71d497129d35b634404d46f7b07c6
terratenff <49789537+terratenff@users.noreply.github.com>
2020-05-23 20:29:30 +0300

    Added documentation to subject window

--- shipyard/subjectwindow.hh
+++ shipyard/subjectwindow.hh
@@ -8,6 +8,11 @@ namespace Ui {
 class SubjectWindow;
 }
 
+/*!
+ * \class SubjectWindow
+ * \brief Controller for subject-specific settings.
+ * \author terratenff
+ */
 class SubjectWindow : public QWidget
 {
     Q_OBJECT
@@ -19,11 +24,42 @@ public:
 private:
     Ui::SubjectWindow *ui;
 
+    /*!
+     * \var settings_
+     * \brief Application-wide settings. Includes subject-specific
+     * settings.
+     */
     Settings *settings_;
+
+    /*!
+     * \var FACTOR_
+     * \brief Some UI components support integers only, even though
+     * numbers higher than 0 but less than 1 are desired. This
+     * number is used to convert high integer values to actual,
+     * desired values.
+     */
     const double FACTOR_ = 1000;
 private slots:
+
+    /*!
+     * \fn cancel
+     * \brief Undoes any changes made on the user interface.
+     * Also closes the window.
+     */
     void cancel();
+
+    /*!
+     * \fn defaultSettings
+     * \brief Applies default settings to the UI components.
+     */
     void defaultSettings();
+
+    /*!
+     * \fn applyChanges
+     * \brief Saves settings on the UI to the settings instance,
+     * applying them to the rest of the application. Also
+     * closes the window.
+     */
     void applyChanges();
 };
 
