02060653738b69b4581d58c0e5252597b2013e9b
terratenff <49789537+terratenff@users.noreply.github.com>
2020-05-28 19:59:11 +0300

    Implemented unit tests for fitness functions

--- unit-tests/test_fitness.hh
+++ unit-tests/test_fitness.hh
@@ -17,42 +17,59 @@ public:
     TestFitness();
     ~TestFitness();
 
-private:
-
-    /*!
-     * \fn help
-     * \brief TODO
-     */
-    void help();
 private slots:
 
     /*!
-     * \brief TODO
+     * \brief Tests fitness function "correct_angle".
+     *
+     * Testing consists of comparing instances to one another:
+     * instance A should be of greater fitness than instance B,
+     * based on the behaviour that the fitness function describes.
      */
     void test_fitness_correct_angle();
 
     /*!
-     * \brief TODO
+     * \brief Tests fitness function "close_proximity".
+     *
+     * Testing consists of comparing instances to one another:
+     * instance A should be of greater fitness than instance B,
+     * based on the behaviour that the fitness function describes.
      */
     void test_fitness_close_proximity();
 
     /*!
-     * \brief TODO
+     * \brief Tests fitness function "fixed_distance".
+     *
+     * Testing consists of comparing instances to one another:
+     * instance A should be of greater fitness than instance B,
+     * based on the behaviour that the fitness function describes.
      */
     void test_fitness_fixed_distance();
 
     /*!
-     * \brief TODO
+     * \brief Tests fitness function "not_out_of_bounds".
+     *
+     * Testing consists of comparing instances to one another:
+     * instance A should be of greater fitness than instance B,
+     * based on the behaviour that the fitness function describes.
      */
     void test_fitness_not_out_of_bounds();
 
     /*!
-     * \brief TODO
+     * \brief Tests fitness function "look_from_distance".
+     *
+     * Testing consists of comparing instances to one another:
+     * instance A should be of greater fitness than instance B,
+     * based on the behaviour that the fitness function describes.
      */
     void test_fitness_look_from_distance();
 
     /*!
-     * \brief TODO
+     * \brief Tests fitness function "avoid_eye_contact".
+     *
+     * Testing consists of comparing instances to one another:
+     * instance A should be of greater fitness than instance B,
+     * based on the behaviour that the fitness function describes.
      */
     void test_fitness_avoid_eye_contact();
 };
